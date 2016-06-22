e7eb734485718b3bbaea1036a0f9d94dc710191f
Casey Bennington <cbenning@vt.edu>
2016-06-22 04:09:34 -0700

    Mailer adjustments

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,16 +1,16 @@
-# if Rails.env.development? || Rails.env.production?
-#   ActionMailer::Base.delivery_method = :smtp
-#   ActionMailer::Base.smtp_settings = {
-#     address: 'smtp.sendgrid.net',
-#     port: '2525',
-#     authentication: :plain,
-#     user_name: ENV['SENDGRID_USERNAME'],
-#     password: ENV['SENDGRID_PASSWORD'],
-#     domain: 'herokuapp.com',
-#     enable_starttls_auto: true
-#   }
-# end
-#
+if Rails.env.development? || Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '2525',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'herokuapp.com',
+    enable_starttls_auto: true
+  }
+end
+# 
 # ActionMailer::Base.smtp_settings = {
 #   port: 587,
 #   address: 'smtp.mailgun.org',
@@ -22,22 +22,3 @@
 #   content_type: 'text/html'
 # }
 # ActionMailer::Base.delivery_method = :smtp
-#
-# # Makes debugging *way* easier
-# ActionMailer::Base.raise_delivery_errors = true
-
-# # This interceptor just makes sure that local mail only emails you.
-# # http://edgeguides.rubyonrails.org/action_mailer_basics.html#intercepting-emails
-# class DevelopmentMailInterceptor
-#   def self.delivering_email(message)
-#     message.to = 'cbenning@vt.edu'
-#     message.cc = nil
-#     message.bcc = nil
-#   end
-# end
-#
-# # Locally, outgoing mail will be 'intercepted' by the
-# # above DevelopmentMailInterceptor before going out
-# if Rails.env.development?
-#   ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
-# end
